5435cf49bddabb6ff318280f0859f2080783bc03
RodrigoGomes <rodrigo_c_gomes@estudante.sesisenai.org.br>
2023-03-14 19:32:36 -0300

    incluido estilo, busca do poster e acertado o busca

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ document
 
 const filmes = [];
 
+document.getElementById("pesquisa-input").addEventListener("input", (e) => {
+  filtarFilmes(e.target.value);
+});
+
 function registrarFilme() {
   const filme = pegaDadosInput();
   if (filmesRegistrados(filme.titulo))
@@ -12,7 +16,7 @@ function registrarFilme() {
   alert("Filme cadastrado com sucesso!");
 
   limpaFormulario();
-  listarFilmes();
+  listarFilmes(filmes);
 }
 
 function pegaDadosInput() {
@@ -33,16 +37,16 @@ function filmesRegistrados(titulo) {
   return filmes.find((filme) => filme.titulo === titulo);
 }
 
-function listarFilmes() {
-  document.getElementById("conteiner-filmes").innerHTML = filmes
-    .map(
-      (filme) => `
+async function listarFilmes(filmes) {
+  const arrayHTML = await Promise.all(
+    filmes.map(
+      async (filme) => `
    <div class="cartao">
           <div class="imagem-filme">
-            <img src="/imagem/no_image.jpg" alt="Poster-filme" width="200px" />
+            <img src="${await pegarPoster(filme.titulo)}" />
           </div>
           <div class="detalhes-filme">
-            <p>${filme.titulo}</p>
+            <h3>${filme.titulo}</h3>
             <p class="nota">Nota: <span>${filme.nota}</span></p>
             <p class="duracao-filme">
               Duração: <span>${filme.duracaoFilme}</span>
@@ -51,5 +55,28 @@ function listarFilmes() {
         </div>
    `
     )
-    .join("");
+  );
+  document.getElementById("conteiner-filmes").innerHTML = arrayHTML.join("");
+}
+function filtarFilmes(filtro) {
+  console.log(filtro);
+  const filmesFiltrados = filmes.filter((filme) =>
+    filtro ? filme.titulo.toLowerCase().includes(filtro.toLowerCase()) : true
+  );
+
+  if (!filmesFiltrados.length)
+    alert("Não foi encontrado nenhum filme com esse título");
+
+  listarFilmes(filmesFiltrados.length ? filmesFiltrados : filmes);
+}
+
+async function pegarPoster(tituloFilme) {
+  const url = "http://image.tmdb.org/t/p/w500";
+  const imgPoster = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=${tituloFilme}`
+  );
+
+  const json = await imgPoster.json();
+  console.log(json);
+  return json.results.length ? url + json.results[0].poster_path : "/imagem/no_image.jpg";
 }
